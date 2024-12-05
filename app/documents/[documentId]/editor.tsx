f88a70474940546f055c133d3a5663613973ee80
Candra Wali Sanjaya <82704254+chndrwali@users.noreply.github.com>
2024-12-05 13:06:58 +0700

    add: image button

--- app/documents/[documentId]/editor.tsx
+++ app/documents/[documentId]/editor.tsx
@@ -15,6 +15,7 @@ import FontFamily from '@tiptap/extension-font-family';
 import TextStyle from '@tiptap/extension-text-style';
 import { Color } from '@tiptap/extension-color';
 import Highlight from '@tiptap/extension-highlight';
+import Link from '@tiptap/extension-link';
 import { useEditorStore } from '@/store/use-editor-store';
 
 export const Editor = () => {
@@ -69,6 +70,11 @@ export const Editor = () => {
       Color,
       Highlight.configure({
         multicolor: true,
+      }),
+      Link.configure({
+        openOnClick: false,
+        autolink: true,
+        defaultProtocol: 'https',
       }),
     ],
     content: `<table>
