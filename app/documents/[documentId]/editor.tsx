7836d59ba580c019e2235e874877a96cdf9cbabc
Candra Wali Sanjaya <82704254+chndrwali@users.noreply.github.com>
2024-12-05 13:33:24 +0700

    add: list button and align button

--- app/documents/[documentId]/editor.tsx
+++ app/documents/[documentId]/editor.tsx
@@ -16,6 +16,7 @@ import TextStyle from '@tiptap/extension-text-style';
 import { Color } from '@tiptap/extension-color';
 import Highlight from '@tiptap/extension-highlight';
 import Link from '@tiptap/extension-link';
+import TextAlign from '@tiptap/extension-text-align';
 import { useEditorStore } from '@/store/use-editor-store';
 
 export const Editor = () => {
@@ -67,6 +68,9 @@ export const Editor = () => {
       Underline,
       FontFamily,
       TextStyle,
+      TextAlign.configure({
+        types: ['heading', 'paragraph'],
+      }),
       Color,
       Highlight.configure({
         multicolor: true,
