de73aa9bda3c68db3f5e464acb91ab79f08a59c1
Candra Wali Sanjaya <82704254+chndrwali@users.noreply.github.com>
2024-12-05 20:26:28 +0700

    feat(home, navbar, search input)

--- app/documents/[documentId]/ruler..tsx
+++ app/documents/[documentId]/ruler..tsx
@@ -58,8 +58,8 @@ export const Ruler = () => {
     setRightMargin(56);
   };
   return (
-    <div ref={rulerRef} onMouseMove={handleMouseMove} onMouseUp={handleMouseUp} onMouseLeave={handleMouseUp} className="h-6 border-b border-gray-300 flex items-end relative select-none print:hidden">
-      <div id="ruler-container" className="max-w-[816px] mx-auto  w-full h-full relative">
+    <div ref={rulerRef} onMouseMove={handleMouseMove} onMouseUp={handleMouseUp} onMouseLeave={handleMouseUp} className="w-[816px] mx-auto h-6 border-b border-gray-300 flex items-end relative select-none print:hidden">
+      <div id="ruler-container" className="w-full h-full relative">
         <Marker position={leftMargin} isLeft={true} isDragging={isDraggingLeft} onMouseDown={handleLeftMouseDown} onDoubleClick={handleLeftDoubleClick} />
         <Marker position={rightMargin} isLeft={false} isDragging={isDraggingRight} onMouseDown={handleRightMouseDown} onDoubleClick={handleRightDoubleClick} />
         <div className="absolute inset-x-0 bottom-0 h-full">
