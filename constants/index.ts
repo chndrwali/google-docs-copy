7b7ef89ce3c0da151a3841c000f9cddf8d93669e
Candra Wali Sanjaya <82704254+chndrwali@users.noreply.github.com>
2024-12-05 20:56:57 +0700

    add: template gallery

---
+++ constants/index.ts
@@ -0,0 +1,37 @@
+export const templates = [
+  {
+    id: 'blank',
+    label: 'Blank Document',
+    imageUrl: '/images/blank-document.svg',
+  },
+  {
+    id: 'software-proposal',
+    label: 'Software Proposal',
+    imageUrl: '/images/software-proposal.svg',
+  },
+  {
+    id: 'resume',
+    label: 'Resume',
+    imageUrl: '/images/resume.svg',
+  },
+  {
+    id: 'project-proposal',
+    label: 'Project Proposal',
+    imageUrl: '/images/project-proposal.svg',
+  },
+  {
+    id: 'letter',
+    label: 'Letter',
+    imageUrl: '/images/letter.svg',
+  },
+  {
+    id: 'cover-letter',
+    label: 'Cover Letter',
+    imageUrl: '/images/cover-letter.svg',
+  },
+  {
+    id: 'business-letter',
+    label: 'Business letter',
+    imageUrl: '/images/business-letter.svg',
+  },
+];
