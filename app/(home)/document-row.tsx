2ee11f3844bd1036266f8fe9876823b4fa54af49
Candra Wali Sanjaya <82704254+chndrwali@users.noreply.github.com>
2024-12-06 16:07:51 +0700

    add: rename and remove dialog

--- app/(home)/document-row.tsx
+++ app/(home)/document-row.tsx
@@ -1,17 +1,22 @@
+'use client';
+
 import { SiGoogledocs } from 'react-icons/si';
 import { TableCell, TableRow } from '@/components/ui/table';
 import { Doc } from '@/convex/_generated/dataModel';
-import { BuildingIcon, CircleUserIcon, MoreVertical } from 'lucide-react';
+import { BuildingIcon, CircleUserIcon } from 'lucide-react';
 import { format } from 'date-fns';
-import { Button } from '@/components/ui/button';
+import { DocumentMenu } from './document-menu';
+import { useRouter } from 'next/navigation';
 
 interface DocumentRowProps {
   document: Doc<'documents'>;
 }
 
 export const DocumentRow = ({ document }: DocumentRowProps) => {
+  const router = useRouter();
+
   return (
-    <TableRow className="cursor-pointer">
+    <TableRow className="cursor-pointer" onClick={() => router.push(`/documents/${document._id}`)}>
       <TableCell className="w-[50px]">
         <SiGoogledocs className="size-6 fill-blue-500" />
       </TableCell>
@@ -22,9 +27,7 @@ export const DocumentRow = ({ document }: DocumentRowProps) => {
       </TableCell>
       <TableCell className="text-muted-foreground hidden md:table-cell">{format(new Date(document._creationTime), 'MMM dd, yyyy')}</TableCell>
       <TableCell className="flex justify-end">
-        <Button variant="ghost" size="icon" className="rounded-full">
-          <MoreVertical className="size-4" />
-        </Button>
+        <DocumentMenu documentId={document._id} title={document.title} onNewTabClick={() => window.open(`/documents/${document._id}`, '_blank')} />
       </TableCell>
     </TableRow>
   );
---
+++ components/rename-dialog.tsx
@@ -0,0 +1,65 @@
+'use client';
+
+import { api } from '@/convex/_generated/api';
+import { Id } from '@/convex/_generated/dataModel';
+import { useMutation } from 'convex/react';
+import { useState } from 'react';
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+
+interface RenameDialogProps {
+  documentId: Id<'documents'>;
+  children: React.ReactNode;
+  initialTitle: string;
+}
+
+export const RenameDialog = ({ documentId, children, initialTitle }: RenameDialogProps) => {
+  const update = useMutation(api.documents.updateById);
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [title, setTitle] = useState(initialTitle);
+  const [open, setOpen] = useState(false);
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsUpdating(true);
+
+    update({ id: documentId, title: title.trim() || 'Untitled' }).finally(() => {
+      setIsUpdating(false);
+      setOpen(false);
+    });
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>{children}</DialogTrigger>
+      <DialogContent onClick={(e) => e.stopPropagation()}>
+        <form onSubmit={onSubmit}>
+          <DialogHeader>
+            <DialogTitle>Rename document</DialogTitle>
+            <DialogDescription>enter a new name for this document</DialogDescription>
+          </DialogHeader>
+          <div className="my-4">
+            <Input value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Document name" onClick={(e) => e.stopPropagation()} />
+          </div>
+          <DialogFooter>
+            <Button
+              type="button"
+              variant="ghost"
+              disabled={isUpdating}
+              onClick={(e) => {
+                e.stopPropagation();
+                setOpen(false);
+              }}
+            >
+              Cancel
+            </Button>
+            <Button type="submit" disabled={isUpdating} onClick={(e) => e.stopPropagation()}>
+              Save
+            </Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
