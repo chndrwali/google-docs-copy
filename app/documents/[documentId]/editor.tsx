61333bc66c8840bac3f380c7ce05add161c504bd
Candra Wali Sanjaya <82704254+chndrwali@users.noreply.github.com>
2024-12-07 15:20:25 +0700

    add: margin dinamyc

--- app/documents/[documentId]/editor.tsx
+++ app/documents/[documentId]/editor.tsx
@@ -20,10 +20,14 @@ import TextAlign from '@tiptap/extension-text-align';
 import { useEditorStore } from '@/store/use-editor-store';
 import { FontSizeExtension } from '@/extensions/font-size';
 import { useLiveblocksExtension } from '@liveblocks/react-tiptap';
+// import { useStorage } from '@liveblocks/react/suspense';
+import { useStorage } from '@liveblocks/react';
 import { Ruler } from './ruler.';
 import { Threads } from './threads';
 
 export const Editor = () => {
+  const leftMargin = useStorage((root) => root.leftMargin);
+  const rightMargin = useStorage((root) => root.rightMargin);
   const { setEditor } = useEditorStore();
   const liveblocks = useLiveblocksExtension();
 
@@ -55,7 +59,7 @@ export const Editor = () => {
     },
     editorProps: {
       attributes: {
-        style: 'padding-left: 56px; padding-right: 56px;',
+        style: `padding-left: ${leftMargin ?? 56}px; padding-right: ${rightMargin ?? 56}px;`,
         class: 'focus:outline-none border print:border-0 bg-white border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text',
       },
     },
